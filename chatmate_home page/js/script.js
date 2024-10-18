30088fc88ae868897df955d6eb8035df750784f1
KawtharDIDI <kawthar.boukahil@univ-constantine2.dz>
2024-10-18 22:51:03 +0100

    Update

--- chatmate_home page/js/script.js
+++ chatmate_home page/js/script.js
@@ -1,16 +1,1 @@
-<script>
-  const text = "Connect Minds. Share Ideas."; // The text to display
-  const h1 = document.getElementById("typing"); // Target the h1 element
-  let index = 0;
 
-  function typeCharacter() {
-    if (index < text.length) {
-      h1.textContent += text.charAt(index);// Add one character at a time
-      index++;
-      setTimeout(typeCharacter, 100); // Adjust speed by changing the timeout
-    }
-}
-
-  // Start the typing animation when the page loads
-  window.onload = typeCharacter;
-</script>
